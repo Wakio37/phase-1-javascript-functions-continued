d7d4fd2bc1d0a69f8106380feba13fc18a7e5a28
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-07-23 12:48:07 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+console.log(saturdayFun("play soccer"))
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork("I will work from home"))
+
+
+function wrapAdjective(highlight){
+    return function inner(message = "special"){
+        return `You are ${highlight}${message}${highlight}!`
+    }
+
+}
+
+console.log(wrapAdjective("*")("message"))
